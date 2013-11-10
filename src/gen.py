b7f55bfe6d1a4dd0d0ade01e5030c468f8be82e6
Daniel Goldbach <dan.goldbach@gmail.com>
2013-11-10 13:24:49 +1100

    Clean gen.py code and use command line arguments

--- src/gen.py
+++ src/gen.py
@@ -2,50 +2,67 @@
 gen.py
 Daniel Goldbach
 
-Generates training and test sets from the Netflix challenge data. The test set
-contains data from the probe set. The challenge data combines all ratings into
-the training set, which doesn't make sense to me. Better to have a clear
-separation between training and test data.
+Generates disjoint training and test sets from the Netflix challenge data. The
+test set contains data from Netflix's probe set. The original challenge data
+combines all ratings into the training set, which doesn't make sense to me.
+Better to have a clear separation between training and test data.
 
 """
 
-
-from collections import defaultdict
-import sys
+import argparse
+import collections
 import os
 
-root = '..'
-training_set = 'training_set'
+ROOT = '..'
 
-MIDS = range(1, int(sys.argv[1])+1)
 
-f = open(os.path.join(root, 'probe.txt'))
-probe = defaultdict(list)
-mid = None
-for line in f:
-    if ':' in line:
-        mid = int(line.split(':')[0])
-    else:
-        uid = int(line)
-        probe[mid].append(uid)
-
-w = open(os.path.join(root, 'probe_rated_{}_{}.txt'.format(training_set, len(MIDS))), 'w')
-
-for mid in MIDS:
-    name = 'mv_{:0>7}.txt'.format(mid)
-
-    if mid in probe:
-        w.write('%d:\n' % mid)
-
-    f = open(os.path.join(root, training_set, name))
-    tf = open(os.path.join(root, 'noprobe', training_set, name), 'w')
-    tf.write(f.readline())
+def generate(mids, training_set):
+    f = open(os.path.join(ROOT, 'probe.txt'))
+    probe = collections.defaultdict(list)
+    mid = None
     for line in f:
-        u, r, _ = line.split(',')
-        u = int(u)
-        r = int(r)
-        if u in probe[mid]:
-            w.write(line)
+        if ':' in line:
+            mid = int(line.split(':')[0])
         else:
-            tf.write(line)
+            uid = int(line)
+            probe[mid].append(uid)
 
+    w = open(os.path.join(ROOT, 'probe_rated.txt'), 'w')
+
+    for mid in mids:
+        name = 'mv_{:0>7}.txt'.format(mid)
+
+        if mid in probe:
+            w.write('%d:\n' % mid)
+
+        f = open(os.path.join(ROOT, training_set, name))
+        tf = open(os.path.join(ROOT, 'noprobe', training_set, name), 'w')
+        tf.write(f.readline()) # first line contains movie ID then ':'
+        for line in f:
+            u, r, _ = line.split(',')
+            u, r = int(u), int(r)
+
+            # Write instance into either training set or test set based on type
+            if u in probe[mid]:
+                w.write(line)
+            else:
+                tf.write(line)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Generate formatted and appropriately sized training sets.')
+    parser.add_argument(
+        'num_movies',
+        type=int,
+        help='the number of movies in the dataset to process. 1 <= M <= 17770.')
+    parser.add_argument(
+        '-f', '--fulldataset',
+        action='store_true',
+        help='use the full dataset as opposed to the medium dataset.')
+    args = parser.parse_args()
+
+    training_set_fn = 'training_set' if args.fulldataset else 'training_med'
+    movie_ids = range(1, args.num_movies + 1)
+
+    generate(movie_ids, training_set_fn)
