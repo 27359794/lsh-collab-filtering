620d926b82954537fe25f63a51720007f3e8eb7e
Daniel Goldbach <dan.goldbach@gmail.com>
2013-11-30 16:21:33 +1100

    Clean up gen.py

--- src/gen.py
+++ src/gen.py
@@ -1,13 +1,9 @@
 """
 gen.py
 Daniel Goldbach
+2013 October
 
-Generates disjoint and nicely formatted training and test sets from the Netflix
-challenge data. The test set contains data from Netflix's probe set. The
-original challenge data combines all ratings into the training set, which
-doesn't make sense to me. Better to have a clear separation between training and
-test data.
-
+Generate properly formatted datasets.
 """
 
 import argparse
@@ -17,46 +13,14 @@ import os
 ROOT = '..'
 
 
-def generate(mids, training_set):
-    f = open(os.path.join(ROOT, 'probe.txt'))
-    probe = collections.defaultdict(list)
-    mid = None
-    for line in f:
-        if ':' in line:
-            mid = int(line.split(':')[0])
-        else:
-            uid = int(line)
-            probe[mid].append(uid)
-
-    w = open(os.path.join(ROOT, 'probe_rated.txt'), 'w')
-
-    for mid in mids:
-        name = 'mv_{:0>7}.txt'.format(mid)
-
-        if mid in probe:
-            w.write('%d:\n' % mid)
-
-        f = open(os.path.join(ROOT, training_set, name))
-        tf = open(os.path.join(ROOT, 'no_probe_set/', name), 'w')
-        tf.write(f.readline()) # first line contains movie ID then ':'
-        for line in f:
-            u, r, _ = line.split(',')
-            u, r = int(u), int(r)
-
-            # Write instance into either training set or test set based on type
-            if u in probe[mid]:
-                w.write(line)
-            else:
-                tf.write(line)
-
-
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(
         description='Generate formatted and appropriately sized training sets.')
     parser.add_argument(
         'num_movies',
         type=int,
-        help='the number of movies to include in the datasets. 1 <= num_movies <= 17770.')
+        help='the number of movies to include in the datasets. '
+             '1 <= num_movies <= 17770.')
     parser.add_argument(
         '-f', '--fulldataset',
         action='store_true',
@@ -69,3 +33,55 @@ if __name__ == '__main__':
     generate(movie_ids, training_set_fn)
 
     print 'successfully generated datasets.'
+
+
+def generate(mids, training_set):
+    """
+    Generates disjoint and nicely formatted training and test sets from the
+    Netflix challenge data. The test set contains data from Netflix's probe set.
+    The original challenge data combines all ratings into the training set,
+    which doesn't make sense to me. Better to have a clear separation between
+    training and test data.
+
+    @param mids
+        list of movie IDs to include in generated sets
+    @param training_set
+        str path to directory containing training data per movie
+
+    """
+
+    probe_file = open(os.path.join(ROOT, 'probe.txt'))
+    probe = collections.defaultdict(list)
+    mid = None
+    for line in probe_file:
+        if ':' in line:
+            mid = int(line.split(':')[0])
+        else:
+            uid = int(line)
+            probe[mid].append(uid)
+
+    formatted_probe_file = open(os.path.join(ROOT, 'probe_rated.txt'), 'w')
+
+    for mid in mids:
+        name = 'mv_{:0>7}.txt'.format(mid)
+
+        if mid in probe:
+            formatted_probe_file.write('%d:\n' % mid)
+
+        training_in_file = open(os.path.join(ROOT, training_set, name))
+        training_out_file = open(os.path.join(ROOT, 'no_probe_set/', name), 'w')
+
+        # First line always contains movie ID then ':'
+        training_out_file.write(training_in_file.readline())
+        for line in training_in_file:
+            uid = int(line.split(',')[0])
+
+            # Write instance into either training set or test set based on type
+            if uid in probe[mid]:
+                formatted_probe_file.write(line)
+            else:
+                training_out_file.write(line)
+
+
+if __name__ == '__main__':
+    main()
